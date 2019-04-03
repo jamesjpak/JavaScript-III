06a072925faebac26b930c769b139e1281dd2e64
EoPlex <you@example.com>
2019-04-03 12:43:03 -0700

    started and working on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,37 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global binding is the principle of when 'this' is utilized in the global scope, 
+    'this' refers to the window Object (in the browser this is the directory for HTML).
+
+* 2. Implicit binding is the principle that in the situation a function is called using dot notation, 'this' 
+     refers to the object before the dot.  
+
+* 3. When utilizing new binding, the 'new' keyword creates a new object, and '
+     this' is assigned to the newly created object.
+
+* 4. The principle of explicit binding is specifying 'this' to an object through the call or apply methods.  
+     Any previous assignment to 'this' will be overridden and reassigned to the specified object.  
+
+
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
 
+
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
 
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
 
+
+// Principle 4
 // code example for Explicit Binding
